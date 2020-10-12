6b0379aed605280009a6c30947479c80dea922cc
josedevian <jdevian98@gmail.com>
2020-10-12 16:10:03 +0700

    initial commit lab-4

---
+++ activity1/app/src/main/java/id/ac/ui/cs/mobileprogramming/activity1/DetailsFragment.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.mobileprogramming.activity1;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.lifecycle.ViewModelProvider;
+import id.ac.ui.cs.mobileprogramming.activity1.databinding.FragmentDetailsBinding;
+
+public class DetailsFragment extends Fragment {
+    private FragmentDetailsBinding binding;
+
+    public DetailsFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_details, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SharedViewModel viewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        viewModel.getSelected().observe(getViewLifecycleOwner(), item -> {
+            binding.course.setText(item.getCourse());
+            binding.task.setText(item.getTask());
+            binding.details.setText(item.getDetails());
+        });
+    }
+}
--- activity1/app/src/main/java/id/ac/ui/cs/mobileprogramming/activity1/MainActivity.java
+++ activity1/app/src/main/java/id/ac/ui/cs/mobileprogramming/activity1/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TaskListFragment taskListFragment = new TaskListFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, taskListFragment)
+                .addToBackStack("task_list")
+                .commit();
     }
 }
