78a74c04151b2eed47146b844f73d928cb8c8839
josedevian <jdevian98@gmail.com>
2020-11-30 14:36:29 +0700

    still working on the post to requestbin

--- activity1/app/src/main/java/id/ac/ui/cs/mobileprogramming/activity1/MainActivity.java
+++ activity1/app/src/main/java/id/ac/ui/cs/mobileprogramming/activity1/MainActivity.java
@@ -8,45 +8,53 @@ import android.content.Context;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private final int MY_PERMISSIONS_ACCESS_COARSE_LOCATION = 1;
+    WifiReceiver wifiReceiver;
     private ListView wifiList;
     private WifiManager wifiManager;
-    WifiReceiver wifiReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wifiList = findViewById(R.id.wifi_list);
+        wifiList = findViewById(R.id.list_wifi);
         Button scanBtn = findViewById(R.id.scanBtn);
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
         if (!wifiManager.isWifiEnabled()) {
-            Toast.makeText(this, R.string.wifi_disabled_message, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), R.string.wifi_disabled_message, Toast.LENGTH_LONG).show();
             wifiManager.setWifiEnabled(true);
         }
 
-        scanBtn.setOnClickListener(v -> {
-            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_ACCESS_COARSE_LOCATION);
-            } else {
-                wifiReceiver = new WifiReceiver(wifiManager, wifiList);
-                IntentFilter intentFilter = new IntentFilter();
-                intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-                registerReceiver(wifiReceiver, intentFilter);
-                wifiManager.startScan();
-                Toast.makeText(this, R.string.scanning, Toast.LENGTH_SHORT).show();
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_ACCESS_COARSE_LOCATION);
+                } else {
+                    wifiReceiver = new WifiReceiver(wifiManager, wifiList);
+                    IntentFilter intentFilter = new IntentFilter();
+                    intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+                    MainActivity.this.registerReceiver(wifiReceiver, intentFilter);
+                    wifiManager.startScan();
+                    Toast.makeText(MainActivity.this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+                }
             }
-
         });
     }
 }
--- activity1/app/src/main/java/id/ac/ui/cs/mobileprogramming/activity1/WifiReceiver.java
+++ activity1/app/src/main/java/id/ac/ui/cs/mobileprogramming/activity1/WifiReceiver.java
@@ -5,15 +5,30 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.RequiresApi;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Headers;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 class WifiReceiver extends BroadcastReceiver {
     WifiManager wifiManager;
     ListView wifiDeviceList;
+    ArrayList<String> temp = new ArrayList<>();
+    private final OkHttpClient client = new OkHttpClient();
 
     public WifiReceiver(WifiManager wifiManager, ListView wifiDeviceList) {
         this.wifiManager = wifiManager;
@@ -25,7 +40,6 @@ class WifiReceiver extends BroadcastReceiver {
 
         if(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(action)) {
             List<ScanResult> wifiList = wifiManager.getScanResults();
-            ArrayList<String> temp = new ArrayList<>();
 
             for (ScanResult scanResult : wifiList) {
                 temp.add(scanResult.SSID + " - " + scanResult.capabilities);
@@ -33,6 +47,53 @@ class WifiReceiver extends BroadcastReceiver {
 
             ArrayAdapter arrayAdapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, temp.toArray());
             wifiDeviceList.setAdapter(arrayAdapter);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String makePost() {
+        RequestBody requestBody = null;
+        MultipartBody.Builder multipart = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("Application", "lab-5-tktpl");
+
+        for(int i = 0; i < temp.size(); i++) {
+            multipart.addFormDataPart("Access Point-Name-" + i, temp.get(i));
+        }
+
+        requestBody = multipart.build();
+
+        Request request = new Request.Builder()
+                .url("https://bd90d34b4c34886991d50978fb62a9b2.m.pipedream.net")
+                .post(requestBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            Headers responseHeaders = response.headers();
+            for (int i = 0; i < responseHeaders.size(); i++) {
+                System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+            }
+            return response.body().string();
+        } catch (IOException e) {
+            return "Error:"+ e.getMessage();
+        }
+    }
+
+    class IOAsyncTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                return makePost();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            Log.d("networking", response);
         }
     }
 }
